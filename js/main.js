e9660f0a8bf51e87b1cd544a1d6ee9a2664c2420
Светлана <se.berseneva@gmail.com>
2022-10-09 23:24:47 +0300

    Больше деталей. Четвертый раздел

--- js/main.js
+++ js/main.js
@@ -15,8 +15,11 @@ function getRandomIntInclusive(min, max) {
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+
 }
-getRandomIntInclusive(1, 10);
+
+
+getRandomIntInclusive(1, 25);
 
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна.
@@ -34,58 +37,83 @@ checkMaxString('Текст сообщения', 50);
 // Обьект состоит из пяти ключей (ID, URL, DESCRIPTION,LIKES, COMMENTS)
 
 
-// eslint-disable-next-line no-unused-vars
+const ID = [
+  getRandomIntInclusive(1, 25),
+];
+const URL = [
+  `photos/${ getRandomIntInclusive(1, 25) }.jpg`
+];
 const DESCRIPTION = [
-  'Котик',
-  'Небо',
-  'Собака',
-  'Рука',
-  'Город',
-  'Окно',
-  'Семья',
-  'Улица',
-  'Дерево',
-  'Осень',
-  'Зима',
-  'Лето',
-  'Весна',
-  'Дождь',
-  'Град',
-  'Листья',
-  'Лужа',
-  'Одежда',
-  'Свадьба',
-  'Ужин',
-  'Ресторан',
-  'Посуда',
-  'Карусель',
-  'Фотограф',
-  'Часы',
+  'кот',
+  'собака',
+  'мышь',
+  'утка',
+  'гусь',
+  'курица',
+  'осень',
+  'весна',
+  'лето',
+  'зима',
+  'машина',
+  'мотоцикл',
+  'автобус',
+  'улица',
+  'дом',
+  'утка',
+  'небо',
+  'тарелка',
+  'вилка',
+  'ложка',
+  'чашка',
+  'стакан',
+  'камень',
+  'вода',
+  'земля',
+];
+const LIKES = [
+  getRandomIntInclusive(15, 200),
 ];
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+const COMMENTS = [
+  getRandomIntInclusive(0, 200),
+];
+
+const SIMILAR_WIZARD_COUNT = 4;
+
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
     return NaN;
   }
-  if (min > max) {
-    const newMax = min;
-    min = max;
-    max = newMax;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-getRandomIntInclusive(1, 25);
-
-const createWizard = (num) => {
-  const attributesImg = {
-    'id': num,
-    'url': `photos/${num}.jpg`,
-    'description': `Фото - ${num}`,
-    'likes': getRandomInt(15, 200),
-    'comments': getRandomInt(0, 200)
-  };
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
+// const createWizard = () => {
+//   const randomIDIndex = getRandomPositiveInteger(0, ID.length - 1);
+//   const randomURLIndex = getRandomPositiveInteger(0, URL.length - 1);
+//   const randomDESCRIPTIONIndex = getRandomPositiveInteger(0, DESCRIPTION.length - 1);
+//   const randomLIKESIndex = getRandomPositiveInteger(0, LIKES.length - 1);
+//   const randomCOMMENTSIndex = getRandomPositiveInteger(0, COMMENTS.length - 1);
+//   return {
+//     id: ID[randomIDIndex],
+//     url: URL[randomURLIndex],
+//     description: DESCRIPTION[randomDESCRIPTIONIndex],
+//     likes: LIKES[randomLIKESIndex],
+//     comments: COMMENTS[randomCOMMENTSIndex]
+//   };
+// };
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const object = () => ({
+  id: getRandomArrayElement (ID),
+  url: getRandomArrayElement (URL),
+  description: getRandomArrayElement (DESCRIPTION),
+  likes: getRandomArrayElement (LIKES),
+  comments: getRandomArrayElement (COMMENTS),
+});
+
+const similarWizards = Array.from({length: SIMILAR_WIZARD_COUNT}, object);
+
+console.log(similarWizards);
