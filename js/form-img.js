6665529ca393fd5591f7da6c2a06b2510f8139d1
Светлана <se.berseneva@gmail.com>
2022-10-27 18:20:30 +0300

    8.21. Правда или действие (часть 1)

--- js/form-img.js
+++ js/form-img.js
@@ -4,8 +4,8 @@ import { clearSimilarList } from './picture.js';
 
 const imageForm = document.querySelector('.img-upload__overlay ');
 imageForm.classList.remove('hidden');
-const body = document.querySelector('body');
-body.classList.add('.modal-open');
+// const body = document.querySelector('body');
+// body.classList.add('.modal-open');
 
 
 const userModalOpenElement = document.querySelector('#upload-file');
